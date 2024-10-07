dc5624e94d0f9bd8bcd68e0bf987fffdb0c1c47a
John Rajesh <john1rajesh@gmail.com>
2024-10-07 23:52:57 +0530

    Webpack Starter Kit.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import generateJoke from "./generateJoke";  
+import './styles/main.scss';
+import laughing from "./assets/icons8-rolling-on-the-floor-laughing-94.png";
 
-console.log(generateJoke());
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughing 
+
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
